1b4ab73a7a8857763d0f54533f964a846f1c4645
barimehdi77 <mbari@student.1337.ma>
2023-01-22 00:00:42 +0100

    add kata01

---
+++ Python-Module-00/ex05/kata01.py
@@ -0,0 +1,9 @@
+# Put this at the top of your kata01.py file
+kata = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+}
+
+for lang, creator in kata.items():
+	print(f"{lang} was created by {creator}")
